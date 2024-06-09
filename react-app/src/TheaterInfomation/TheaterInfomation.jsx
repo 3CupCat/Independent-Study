f147af7d3f49e882c259b15fffcce9b1def6d762
3CupCat <chehsen19@gmail.com>
2024-06-09 21:16:27 +0800

    feat: modify cities name && concatenate API

--- react-app/src/TheaterInfomation/TheaterInfomation.jsx
+++ react-app/src/TheaterInfomation/TheaterInfomation.jsx
@@ -1,78 +1,92 @@
-import React from "react";
-import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col, Card, Button, Form } from "react-bootstrap";
 import { useMediaQuery } from "react-responsive";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./TheaterInfomation.css";
+import axios from "axios";
 
 const TheaterInfomation = () => {
   const isLargeScreen = useMediaQuery({ query: "(min-width: 768px)" });
+  const [selectedCity, setSelectedCity] = useState("全部");
+  const [theaterInfomations, setTheaterInfomations] = useState([]);
 
-  const theaterInfomations = [
-    {
-      theaterName: "臺中大遠百威秀影城",
-      theaterImage:
-        "https://lh3.googleusercontent.com/p/AF1QipMDzf1Pnmy5CKC7qeLQLDh23GgD4HkOVs8ny-Ro=s1360-w1360-h1020",
-      address: "台中市西屯區臺灣大道三段251號13樓",
-      tel: "(04)2258-8511",
-    },
-    {
-      theaterName: "台中中港新光影城",
-      theaterImage:
-        "https://lh3.googleusercontent.com/p/AF1QipO7S5HhYmcf-DBpnuS8Rcq_E6ow5gAwPVLVVDhr=s1360-w1360-h1020",
-      address: "台中市西屯區臺灣大道三段301號14樓",
-      tel: "(04)2258-9911",
-    },
-    {
-      theaterName: "台中TIGER CITY威秀影城",
-      theaterImage:
-        "https://lh3.googleusercontent.com/p/AF1QipPbh-XdfxnhpRn0Mtwv-iMhVZiZW2sVJvw1sTt4=s1360-w1360-h1020",
-      address: "台中市西屯區河南路三段120-1號4樓",
-      tel: "(04)3606-5566",
-    },
-    {
-      theaterName: "台中日日新影城",
-      theaterImage:
-        "https://lh3.googleusercontent.com/p/AF1QipNGb5zD0xMWPeMDHEJA3JREpjuDxLIw00s0No4s=s1360-w1360-h1020",
-      address: "台中市中區中華路一段58號",
-      tel: "(04)2222-3138",
-    },
-    {
-      theaterName: "全球影城",
-      theaterImage:
-        "https://lh3.googleusercontent.com/p/AF1QipN0uFOpgvVOJY-5GGAw3tJbTnS_hkMIMhIkRKb0=s1360-w1360-h1020",
-      address: "台中市西區中華路一段1-1號",
-      tel: "(04)2224-2588",
-    },
-    {
-      theaterName: "台中文心秀泰影城",
-      theaterImage:
-        "https://lh3.googleusercontent.com/p/AF1QipNGemRylV8AQP74xuhKvktNvrTaDEHabBsyQF8=s1360-w1360-h1020",
-      address: "台中市南屯區文心南路289號7樓8樓",
-      tel: "(04)2473-5989",
-    },
-    {
-      theaterName: "萬代福影城",
-      theaterImage:
-        "https://lh3.googleusercontent.com/p/AF1QipNpNxjPCa1mFBPjKt_oHutfxeASUDY7ck5_TGyz=s1360-w1360-h1020",
-      address: "台中市中區公園路38號",
-      tel: "(04)2221-0356",
-    },
-    {
-      theaterName: "台中站前秀泰影城",
-      theaterImage:
-        "https://lh3.googleusercontent.com/p/AF1QipN7aWBMsLytwh7FwQHBYWKZ7edXr3wvw94jbF8q=s1360-w1360-h1020",
-      address: "台中市東區南京路76號1樓",
-      tel: "(04)2211-7988",
-    },
-    {
-      theaterName: "台中大魯閣新時代威秀影城",
-      theaterImage:
-        "https://lh3.googleusercontent.com/p/AF1QipOR6buJoxLnnejJ_jq8Ht2ABmNis_xizs0ANW0=s1360-w1360-h1020",
-      address: "台中市東區復興路四段186號4F",
-      tel: "(04)2228-0388",
-    },
+  useEffect(() => {
+    const fetchTheaterInfomations = async () => {
+      try {
+        const response = await axios.get("http://localhost:8080/theaters");
+        setTheaterInfomations(response.data);
+      } catch (error) {
+        if (error.response) {
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          console.log(error.request);
+        } else {
+          console.log("Error", error.message);
+        }
+        console.log(error.config);
+      }
+    };
+
+    fetchTheaterInfomations();
+  }, []);
+
+  // 地區排序
+  const sortedAreas = [
+    "北北基宜",
+    "桃竹苗",
+    "中彰投",
+    "雲嘉南",
+    "高屏澎",
+    "花東",
+    "金馬",
   ];
 
+  // 縣市排序(連江縣 === 馬祖)
+  const cities = [
+    "基隆市",
+    "台北市",
+    "新北市",
+    "桃園市",
+    "新竹市",
+    "新竹縣",
+    "苗栗縣",
+    "台中市",
+    "彰化縣",
+    "南投縣",
+    "雲林縣",
+    "嘉義市",
+    "嘉義縣",
+    "台南市",
+    "高雄市",
+    "屏東縣",
+    "宜蘭縣",
+    "花蓮縣",
+    "台東縣",
+    "澎湖縣",
+    "金門縣",
+    "連江縣",
+  ];
+
+  // 以地區分類影城
+  const groupedTheaters = theaterInfomations.reduce((acc, theater) => {
+    if (!acc[theater.area]) {
+      acc[theater.area] = [];
+    }
+    acc[theater.area].push(theater);
+    return acc;
+  }, {});
+
+  // 尋訪列表中，有影城的縣市
+  const availableCities = cities.map((city) => ({
+    name: city,
+    disabled: !theaterInfomations.some((theater) =>
+      theater.address.startsWith(city)
+    ),
+  }));
+
+  // 利用GoogleMap的QueryParameter達成跳轉功能
   const openGoogleMap = (theaterName) => {
     const url = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
       theaterName
@@ -83,46 +97,71 @@ const TheaterInfomation = () => {
   return (
     <Container className="theat-container">
       <h2 className="title text-center my-4">影城資訊</h2>
-      <Row>
-        {theaterInfomations.map((theaterInfomation, index) => (
-          <Col key={index} md={6} lg={4} className="mb-4">
-            <Card
-              className="theater-card"
-              onClick={
-                isLargeScreen
-                  ? () => openGoogleMap(theaterInfomation.theaterName)
-                  : null
-              }
-            >
-              <div className="card-image-container">
-                <Card.Img
-                  variant="top"
-                  src={theaterInfomation.theaterImage}
-                  alt={theaterInfomation.theaterName}
-                  className="card-image"
-                />
-              </div>
-              <Card.Body>
-                <Card.Title>{theaterInfomation.theaterName}</Card.Title>
-                <Card.Text>
-                  {theaterInfomation.address}
-                  <br />
-                  {theaterInfomation.tel}
-                </Card.Text>
-                {!isLargeScreen && (
-                  <Button
-                    variant="outline-light"
-                    className="float-right"
-                    onClick={() => openGoogleMap(theaterInfomation.theaterName)}
+      <Form.Group controlId="citySelect" className="mb-4">
+        <Form.Label>請選擇地區：</Form.Label>
+        <Form.Select
+          value={selectedCity}
+          onChange={(e) => setSelectedCity(e.target.value)}
+        >
+          <option value="全部">全部</option>
+          {availableCities.map((city) => (
+            <option key={city.name} value={city.name} disabled={city.disabled}>
+              {city.name}
+            </option>
+          ))}
+        </Form.Select>
+      </Form.Group>
+      {sortedAreas.map((area) => (
+        <div key={area} className="mb-4">
+          {selectedCity === "全部" && <h3>{area}</h3>}
+          <Row>
+            {groupedTheaters[area]
+              ?.filter(
+                (theater) =>
+                  selectedCity === "全部" ||
+                  theater.address.startsWith(selectedCity)
+              )
+              .map((theater, index) => (
+                <Col key={index} md={6} lg={4} className="mb-4">
+                  <Card
+                    className="theater-card"
+                    onClick={
+                      isLargeScreen
+                        ? () => openGoogleMap(theater.theaterName)
+                        : null
+                    }
                   >
-                    開啟地圖
-                  </Button>
-                )}
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
+                    <div className="card-image-container">
+                      <Card.Img
+                        variant="top"
+                        src={theater.image}
+                        alt={theater.theaterName}
+                        className="card-image"
+                      />
+                    </div>
+                    <Card.Body>
+                      <Card.Title>{theater.theaterName}</Card.Title>
+                      <Card.Text>
+                        {theater.address}
+                        <br />
+                        {theater.tel}
+                      </Card.Text>
+                      {!isLargeScreen && (
+                        <Button
+                          variant="outline-light"
+                          className="float-right"
+                          onClick={() => openGoogleMap(theater.theaterName)}
+                        >
+                          開啟地圖
+                        </Button>
+                      )}
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
+          </Row>
+        </div>
+      ))}
     </Container>
   );
 };
